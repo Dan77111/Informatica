af3ff871c543fc6ce0b63d1afbc93addac002f65
Daniele Dalla Rosa <Daniele Dalla Rosa>
2016-11-27 15:38:11 +0100

    hbfsbf

---
+++ EsercizioPersona/contattoTelefonicoStudenti.java
@@ -0,0 +1,43 @@
+/**
+ * Aggiungi qui una descrizione della classe agendaTelefonica
+ *
+ * @author Fabio Spiazzi
+ * @version 24/11/2016
+ */
+
+import java.util.*;
+
+public class contattoTelefonicoStudenti {
+    //variabili di istanza
+    private String name;
+    private String surname;
+    private String number;
+    //costruttore
+    public contattoTelefonicoStudenti(String name, String surname, String number) {
+       this.name = name;
+       this.surname = surname;
+       this.number = number;
+    }
+
+    //modifica del numero di telefono
+    public void setNumber(String number) {
+      this.number = number;
+    }
+
+    //modifica del cognome
+    public void setSurname(String surname) {
+      this.surname = surname;
+    }
+
+    //modifica del nome
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    //stampa a video del contatto
+    public void print() {
+      System.out.println("Name: " + name);
+      System.out.println("Surname: " + surname);
+      System.out.println("Number: " + number);
+    }
+}
---
+++ EsercizioPersona/nuovoContattoStudenti.java
@@ -0,0 +1,46 @@
+
+/**
+ * Aggiungi qui una descrizione della classe nuovaAgenda
+ * implementazione della classe agenda telefonica
+ * @author  Daniele Dalla Rosa
+ * @version 24-11-2016
+ */
+
+import java.util.*;
+public class nuovoContattoStudenti{
+   public static void main (){
+      //Istanza della classe
+      contattoTelefonicoStudenti contatto;
+      contatto = new contattoTelefonicoStudenti("Gino", "Pagnotta", "1234567890");
+      //dichiarazione dei dati
+      boolean exit = false;
+      //richiamo dei metodi con gestione delle eccezioni e scelta del metodo da richiamare
+      while (exit == false){
+        System.out.println("Che operazione vuoi eseguire?");
+        System.out.println("modificaNome, modificaCognome, modificaNumero, stampa o exit?");
+        String op = Tastiera.leggiUnaStringa();
+        switch (op){
+          case "modificaNome":
+            System.out.print("Inserire il nome: ");
+            contatto.setName(Tastiera.leggiUnaStringa());
+            break;
+          case "modificaCognome":
+            System.out.print("Inserire il cognome: ");
+            contatto.setSurname(Tastiera.leggiUnaStringa());
+            break;
+          case "modificaNumero":
+            System.out.print("Inserire il numero: ");
+            contatto.setNumber(Tastiera.leggiUnaStringa());
+            break;
+          case "stampa":
+            contatto.print();
+            break;
+          case "exit":
+            exit = true;
+            break;
+          default:
+            System.out.println("Cosa hai scritto?!?");
+      }
+    }
+  }
+}
